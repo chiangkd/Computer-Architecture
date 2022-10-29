b57da4e1f658b60c511f1108238dac2061e91207
chiangkd <chiangkuandi@gmail.com>
2022-10-29 17:56:37 +0800

    Generate radom string and test it with clock function

--- Hw2/hw2.c
+++ Hw2/hw2.c
@@ -1,7 +1,51 @@
 #include<stdio.h>
 #include<time.h>
+#include<stdlib.h>
+#include<string.h>
 
-#define __ARCH_WANT_TIME32_SYSCALLS
+#define MAX_MSG_LEN 1000
+#define GENRAND64(X) (((X) & 0x7F7F7F7F7F7F7F7F) | 0x6060606060606060)
+#define GENRAND8(X)  (((X) & 0x7F) | 0x61)
+
+#define DETECT_NULL(X)         (((X) - 0x0101010101010101) & ~(X) & 0x8080808080808080)
+#define DETECT_CHAR(X, MASK)   (DETECT_NULL((X) ^ (MASK)))
+
+static void GenRandString(char *str, int length)
+{
+    int size = length;
+    uint64_t *lptr = (uint64_t *) str;
+
+    while (size >= 8) {
+        uint64_t rand64 = (uint64_t) rand() << 32 | rand();
+        // printf("in\n");
+        *lptr = GENRAND64(rand64);
+        // if detected no needed character
+        if (DETECT_CHAR(*lptr, 0x6060606060606060) ||\
+            DETECT_CHAR(*lptr, 0x7B7B7B7B7B7B7B7B) ||\
+            DETECT_CHAR(*lptr, 0x7C7C7C7C7C7C7C7C) ||\
+            DETECT_CHAR(*lptr, 0x7D7D7D7D7D7D7D7D) ||\
+            DETECT_CHAR(*lptr, 0x7E7E7E7E7E7E7E7E) ||\
+            DETECT_CHAR(*lptr, 0x7F7F7F7F7F7F7F7F))
+            continue;
+        lptr++;
+        size -= 8;
+    }
+
+    char *cptr = (char *) lptr;
+
+    while (size) {
+        *cptr = GENRAND8(rand());
+        // if detected no needed character
+        if (!(*cptr ^ 0x60) || !(*cptr ^ 0x7B) || !(*cptr ^ 0x7C) || !(*cptr ^ 0x7D) || !(*cptr ^ 0x7E) || !(*cptr ^ 0x7F))
+            continue;
+        cptr++;
+        size--;
+    }   
+    *cptr = '\0';
+}
+
+
+
 int isAnagram(char * s, char * t)
 {
     int letter_freq[26] = {0}; //store frequency of every letter
@@ -22,37 +66,16 @@ int isAnagram(char * s, char * t)
 }
 
 int main() {
-	char *test_1_s = "anagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagram";
-	char *test_1_t = "anagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagramanagram";
+    srand(time(0));
 
-	char *test_2_s = "rat";
-	char *test_2_t = "anagram";
-
-	char *test_3_s = "tseng";
-	char *test_3_t = "gnest";
-
-
-    clock_t time_start = clock();
-    
-    if ( 1 == isAnagram(test_1_s, test_1_t)) {
-        printf("test_1: correct\n");
-    } else {
-        printf("test_1: not_correct\n");
+    char* str = malloc(sizeof(char));
+    for(int i = 10000; i <= 100000; i = i + 1) {
+        GenRandString(str, i);
+        clock_t time_start = clock();
+        if(isAnagram(str, str)) {
+            ;   // print something if needed
+        }
+        clock_t time_end = clock();
+        printf("%d\t%d\n", i, (time_end - time_start) * 1000/CLOCKS_PER_SEC);
     }
-    if ( 0 == isAnagram(test_2_s, test_2_t)) {
-        // printf("test_2: correct\n");
-    } else {
-        // printf("test_2: not_correct\n");
-    }
-    if ( 1 == isAnagram(test_3_s, test_3_t)) {
-        // printf("test_3: correct\n");
-    } else {
-        // printf("test_3: not_correct\n");
-    }
-
-    clock_t time_end = clock();
-
-    fprintf(stderr, "clock gettime time = %d ms\n", (time_end - time_start) * 1000/CLOCKS_PER_SEC);
-
-    
 }
